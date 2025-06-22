56fe2f889636973b4a00b2b4121c735de8920019
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-06-22 22:37:46 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(){
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+    const name = 'Sammy';
+    
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+
+//soln 
+function findMatching(drivers, name) {
+  return drivers.filter(driver =>
+    driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
